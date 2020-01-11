c3d26dd4f84896797e58ade8dae2fbf255f2a2ee
Ashish Tiwari <ashish@Ashishs-MBP.fios-router.home>
2020-01-10 21:08:55 -0500

    Intial Changes

---
+++ Assignment_1/bin/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"Main is printing "<<std::endl;
+    std::cout<<"Main is printing "<<std::endl;
+    std::cout<<"Main is printing "<<std::endl;
+    std::cout<<"Main is printing "<<std::endl;
+    std::cout<<"Main is printing "<<std::endl;
+    return 0;
+}
