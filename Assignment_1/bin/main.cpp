94f9d36fc4ab4d107a4aa178271b6ab46eaf8e09
Ashish Tiwari <ashish@Ashishs-MBP.fios-router.home>
2020-02-04 20:52:13 -0500

    Adding Makefile

--- Assignment_1/bin/main.cpp
+++ Assignment_1/bin/main.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
 
 int main(){
-    std::cout<<"Main is printing "<<std::endl;
-    std::cout<<"Main is printing "<<std::endl;
-    std::cout<<"Main is printing "<<std::endl;
-    std::cout<<"Main is printing "<<std::endl;
-    std::cout<<"Main is printing "<<std::endl;
+    
     return 0;
 }
