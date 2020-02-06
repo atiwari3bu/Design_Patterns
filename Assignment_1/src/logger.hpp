688449a035368f5056e2c4fc44329708300dd9aa
Ashish Tiwari <ashish@Ashishs-MBP.fios-router.home>
2020-02-05 22:29:51 -0500

    Still confused with singleton Class Logger

--- Assignment_1/src/logger.hpp
+++ Assignment_1/src/logger.hpp
@@ -3,8 +3,11 @@ class Singleton_Logger{
     static Singleton_Logger* _Instance;
     static int _DEBUG_LEVEL;
 
+    Singleton_Logger(){} // Constructor made private
+    Singleton_Logger(int temp){} // Constructor made private
+
     public:
-    Singleton_Logger(int DEBUG_LEVEL){ 
+    Singleton_Logger* getInstance(int DEBUG_LEVEL){ 
         if(!_Instance){
             this->_DEBUG_LEVEL=DEBUG_LEVEL;
             _Instance = new Singleton_Logger(DEBUG_LEVEL);
