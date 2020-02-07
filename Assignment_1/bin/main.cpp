c4b5a9890f2f7ca9fb74c3579ff77a6122a66b66
Ashish Tiwari <ashish@Ashishs-MBP.fios-router.home>
2020-02-06 22:38:37 -0500

    Unsuccessful Static object creation

--- Assignment_1/bin/main.cpp
+++ Assignment_1/bin/main.cpp
@@ -2,6 +2,6 @@
 #include"../src/logger.hpp"
 
 int main(){
-    Singleton_Logger* log=new Singleton_Logger(0); 
+    Singleton_Logger::getInstance(0);
     return 0;
 }
--- Assignment_1/src/logger.hpp
+++ Assignment_1/src/logger.hpp
@@ -1,19 +1,25 @@
 class Singleton_Logger{
 
-    static Singleton_Logger* _Instance;
-    static int _DEBUG_LEVEL;
+    static Singleton_Logger* _Instance; 
+    int _DEBUG_LEVEL;
 
     Singleton_Logger(){} // Constructor made private
-    Singleton_Logger(int temp){} // Constructor made private
-
-    public:
-    Singleton_Logger* getInstance(int DEBUG_LEVEL){ 
-        if(!_Instance){
-            this->_DEBUG_LEVEL=DEBUG_LEVEL;
-            _Instance = new Singleton_Logger(DEBUG_LEVEL);
-        }
-
-        return _Instance;
+    Singleton_Logger(int DEBUG_LEVEL){ // Constructor made private
+        _DEBUG_LEVEL=DEBUG_LEVEL;
     }
 
+    public:
+    static Singleton_Logger* getInstance(int DEBUG_LEVEL);
 };
+
+
+
+Singleton_Logger* Singleton_Logger::getInstance(int DEBUG_LEVEL){ 
+
+    if(!_Instance){
+        _Instance = new Singleton_Logger(DEBUG_LEVEL);
+    }
+
+    return _Instance;
+
+}
