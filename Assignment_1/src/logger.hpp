8647dabf53228337fdc9aa98ce7190b2af579716
Ashish Tiwari <ashish@Ashishs-MBP.fios-router.home>
2020-03-12 20:52:25 -0400

    Made variadic template work

--- Assignment_1/src/logger.hpp
+++ Assignment_1/src/logger.hpp
@@ -1,6 +1,7 @@
 #ifndef __LOGGER_HPP__
 #define __LOGGER_HPP__
 #include<string>
+#include<iostream>
 
 enum log_level{
     FATAL,
@@ -24,11 +25,67 @@ class Singleton_Logger{
     static Singleton_Logger* getInstance(log_level DEBUG_LEVEL);
     log_level getDebugLevel();
 
-    template<typename T>
-    void print(T t);
+    void printOnScreen();
+
     template<typename T,typename... Ts>
-    void printOnScreen(T &&first, Ts&&... rest);
+    void printOnScreen(T first, Ts... rest);
 
 };
+
+Singleton_Logger* Singleton_Logger::_Instance=nullptr;
+
+Singleton_Logger* Singleton_Logger::getInstance(log_level DEBUG_LEVEL){ 
+    if(!_Instance){
+        _Instance = new Singleton_Logger(DEBUG_LEVEL);
+    }
+
+    return _Instance;
+
+}
+
+log_level Singleton_Logger::getDebugLevel(){
+    return this->_DEBUG_LEVEL;
+}
+
+void Singleton_Logger::printOnScreen(){
+    std::cout<<"\n"<<"----------------END---------------"<<std::endl;
+}
+
+template<typename T,typename... Ts>
+void Singleton_Logger::printOnScreen(T first, Ts... rest){
+
+
+    if(this->_DEBUG_LEVEL<=log_level::FATAL){// Going for Debug level
+        std::cout<<first<<" ";
+        printOnScreen(rest...);
+
+    }
+    /*
+
+    if(this->_DEBUG_LEVEL<=log_level::ERROR){// Going for Debug level
+        std::cout<<first<<" ";
+        printOnScreen(rest...);
+
+    }
+
+    if(this->_DEBUG_LEVEL<=log_level::ERROR){// Going for Debug level
+        std::cout<<first<<" ";
+        printOnScreen(rest...);
+
+    }
+
+    if(this->_DEBUG_LEVEL<=log_level::ERROR){// Going for Debug level
+        std::cout<<first<<" ";
+        printOnScreen(rest...);
+
+    }
+
+    if(this->_DEBUG_LEVEL<=log_level::ERROR){// Going for Debug level
+        std::cout<<first<<" ";
+        printOnScreen(rest...);
+
+    }
+    */
+}
 
 #endif
--- Assignment_1/src/main.cpp
+++ Assignment_1/src/main.cpp
@@ -4,9 +4,6 @@
 
 int main(){
     Singleton_Logger* logger =  Singleton_Logger::getInstance(ERROR); 
-    std::vector<int> v;
-    v.push_back(3);
-    v.push_back(4);
     logger->printOnScreen(12,34,5);
 
     return 0;
